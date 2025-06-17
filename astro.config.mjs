38c2ac0c59df06e30408d37d06bfda0161ec8170
DominicDev <s0l0estudi4r@gmail.com>
2025-06-17 13:00:07 +0200

    v0.0.6

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,9 @@ export default defineConfig({
     assets: 'static'
   },
   //Deploy 20 Projects
-  base: '/20-projects-with-socket-io/',
-  site: 'https://20essentials.github.io/20-projects-with-socket-io/'
+  // base: '/20-projects-with-socket-io/',
+  // site: 'https://20essentials.github.io/20-projects-with-socket-io/'
 
   //Localhost
-  // site: 'http://localhost:4321/'
+  site: 'http://localhost:4321/'
 });
